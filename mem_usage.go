611e7ea912df8bd263e38bb0cb56e1ff13e690ca
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-08-15 16:56:42 -0600

    remove logging

--- mem_usage.go
+++ mem_usage.go
@@ -1,16 +1,12 @@
 package cache
 
 import (
-	"log"
-
 	"github.com/pbnjay/memory"
 )
 
 // returns a value between 0 and 1 representing the current percentage of memory used
 func MemUsage() float64 {
 	totalMemory := memory.TotalMemory()
-	log.Println(totalMemory)
 	freeMemory := memory.FreeMemory()
-	log.Println(freeMemory)
 	return 1 - (float64(freeMemory) / float64(totalMemory))
 }
--- mem_usage_test.go
+++ mem_usage_test.go
@@ -1,14 +1,15 @@
 package cache
 
 import (
-	"log"
 	"testing"
 )
 
 func TestMemUsage(t *testing.T) {
 	memUsage := MemUsage()
-	log.Println(memUsage)
-	if memUsage == 0 {
-		t.Fatal("MemUsage() should not return 0")
+	if memUsage <= 0 {
+		t.Fatal("MemUsage() should not return <= 0")
+	}
+	if memUsage > 1 {
+		t.Fatal("MemUsage() should not return > 1")
 	}
 }
