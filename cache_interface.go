b6cfaab4bf913efafc473201f9cdc901f03da6da
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-08-16 15:14:18 -0600

    added wrapper helpers

--- cache_interface.go
+++ cache_interface.go
@@ -1,7 +1,7 @@
 package cache
 
 type Cache[KEY_TYPE comparable, VALUE_TYPE any] interface {
-	Get(key KEY_TYPE) (VALUE_TYPE, bool)
 	Set(key KEY_TYPE, value VALUE_TYPE)
+	Get(key KEY_TYPE) (VALUE_TYPE, bool)
 	Remove(key KEY_TYPE)
 }
