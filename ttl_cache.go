a6f77034d03a6f2d7ad4540fa0ebadf478d71a43
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-08-27 15:17:47 -0600

    using some cleaner logic from go-deduplicate

--- ttl_cache.go
+++ ttl_cache.go
@@ -3,11 +3,17 @@ package cache
 import (
 	"sync"
 	"time"
+
+	"github.com/nuvi/unicycle"
 )
 
 type ttlCacheValue[VALUE_TYPE any] struct {
 	createdAt time.Time
 	value     VALUE_TYPE
+}
+
+func (cached *ttlCacheValue[VALUE_TYPE]) isExpired(ttl time.Duration) bool {
+	return cached.createdAt.Before(time.Now().Add(-ttl))
 }
 
 // a cache that stores values for a fixed amount of time
@@ -16,7 +22,7 @@ type TTLCache[KEY_TYPE comparable, VALUE_TYPE any] struct {
 	ttl           time.Duration
 	reapFrequency time.Duration
 	lock          *sync.RWMutex
-	done          chan bool
+	canceller     func()
 }
 
 func NewTTLCache[KEY_TYPE comparable, VALUE_TYPE any](ttl, reapFrequency time.Duration) *TTLCache[KEY_TYPE, VALUE_TYPE] {
@@ -25,7 +31,6 @@ func NewTTLCache[KEY_TYPE comparable, VALUE_TYPE any](ttl, reapFrequency time.Du
 		ttl:           ttl,
 		reapFrequency: reapFrequency,
 		lock:          &sync.RWMutex{},
-		done:          make(chan bool),
 	}
 
 	cache.StartReaping()
@@ -42,11 +47,22 @@ func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) Set(key KEY_TYPE, value VALUE_TY
 	}
 }
 
-func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) Get(key KEY_TYPE) (VALUE_TYPE, bool) {
+func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) rawGet(key KEY_TYPE) (ttlCacheValue[VALUE_TYPE], bool) {
 	ttlCache.lock.RLock()
 	defer ttlCache.lock.RUnlock()
-	element, ok := ttlCache.cache[key]
-	return element.value, ok
+	cached, ok := ttlCache.cache[key]
+	return cached, ok
+}
+
+func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) Get(key KEY_TYPE) (VALUE_TYPE, bool) {
+	cached, ok := ttlCache.rawGet(key)
+	if ok {
+		if cached.isExpired(ttlCache.ttl) {
+			ok = false
+			go ttlCache.Remove(key)
+		}
+	}
+	return cached.value, ok
 }
 
 func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) Remove(key KEY_TYPE) {
@@ -56,30 +72,18 @@ func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) Remove(key KEY_TYPE) {
 }
 
 func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) StartReaping() {
-	go func() {
-		ticker := time.NewTicker(ttlCache.reapFrequency)
-		select {
-		case <-ticker.C:
-			ttlCache.Reap()
-		case <-ttlCache.done:
-			ticker.Stop()
-			return
-		}
-	}()
+	ttlCache.canceller = unicycle.Repeat(ttlCache.Reap, ttlCache.reapFrequency, false)
 }
 
 func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) StopReaping() {
-	go func() {
-		ttlCache.done <- true
-	}()
+	ttlCache.canceller()
 }
 
 func (ttlCache *TTLCache[KEY_TYPE, VALUE_TYPE]) Reap() {
 	ttlCache.lock.Lock()
 	defer ttlCache.lock.Unlock()
-	expired := time.Now().Add(-ttlCache.ttl)
-	for key, element := range ttlCache.cache {
-		if expired.After(element.createdAt) {
+	for key, cached := range ttlCache.cache {
+		if cached.isExpired(ttlCache.ttl) {
 			// Note: deleting from a map doesn't shrink the underlying array, so while memory can be re-used it is not freed
 			delete(ttlCache.cache, key)
 		}
