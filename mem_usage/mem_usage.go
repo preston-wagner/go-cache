0ce73386751045d28277b7edb0d5cb2dcbf9783c
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-10-30 11:40:56 -0600

    added dependency to check memory usage (for usage in smart caches)

---
+++ mem_usage/mem_usage.go
@@ -0,0 +1,12 @@
+package mem_usage
+
+import (
+	"github.com/pbnjay/memory"
+)
+
+// returns a value between 0 and 1 representing the current percentage of memory used
+func MemUsage() float64 {
+	totalMemory := memory.TotalMemory()
+	freeMemory := memory.FreeMemory()
+	return 1 - (float64(freeMemory) / float64(totalMemory))
+}
---
+++ mem_usage/mem_usage_test.go
@@ -0,0 +1,15 @@
+package mem_usage
+
+import (
+	"testing"
+)
+
+func TestMemUsage(t *testing.T) {
+	memUsage := MemUsage()
+	if memUsage <= 0 {
+		t.Fatal("MemUsage() should not return <= 0")
+	}
+	if memUsage > 1 {
+		t.Fatal("MemUsage() should not return > 1")
+	}
+}
