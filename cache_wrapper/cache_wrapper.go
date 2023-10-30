4939f4dae48ebf19935272875f5c76142da56830
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-10-30 11:37:51 -0600

    added WrapWithCacheWithError, test to make sure wrapped functions are only called once

--- cache_wrapper/cache_wrapper.go
+++ cache_wrapper/cache_wrapper.go
@@ -1,6 +1,9 @@
 package cache_wrapper
 
-import "github.com/preston-wagner/go-cache/cache_interface"
+import (
+	"github.com/preston-wagner/go-cache/cache_interface"
+	"github.com/preston-wagner/unicycle/defaults"
+)
 
 func WrapWithCache[KEY_TYPE comparable, VALUE_TYPE any](
 	getter cache_interface.Getter[KEY_TYPE, VALUE_TYPE],
@@ -17,6 +20,7 @@ func WrapWithCache[KEY_TYPE comparable, VALUE_TYPE any](
 	}
 }
 
+// only caches successful calls, not errors
 func WrapWithCacheWithUncachedError[KEY_TYPE comparable, VALUE_TYPE any](
 	getter cache_interface.GetterWithError[KEY_TYPE, VALUE_TYPE],
 	cache cache_interface.Cache[KEY_TYPE, VALUE_TYPE],
@@ -29,6 +33,31 @@ func WrapWithCacheWithUncachedError[KEY_TYPE comparable, VALUE_TYPE any](
 		value, err := getter(key)
 		if err == nil {
 			cache.Set(key, value)
+		}
+		return value, err
+	}
+}
+
+// caches both successful calls and errors
+func WrapWithCacheWithError[KEY_TYPE comparable, VALUE_TYPE any](
+	getter cache_interface.GetterWithError[KEY_TYPE, VALUE_TYPE],
+	cache cache_interface.Cache[KEY_TYPE, VALUE_TYPE],
+	errCache cache_interface.Cache[KEY_TYPE, error],
+) cache_interface.GetterWithError[KEY_TYPE, VALUE_TYPE] {
+	return func(key KEY_TYPE) (VALUE_TYPE, error) {
+		value, ok := cache.Get(key)
+		if ok {
+			return value, nil
+		}
+		err, ok := errCache.Get(key)
+		if ok {
+			return defaults.ZeroValue[VALUE_TYPE](), err
+		}
+		value, err = getter(key)
+		if err == nil {
+			cache.Set(key, value)
+		} else {
+			errCache.Set(key, err)
 		}
 		return value, err
 	}
--- cache_wrapper/cache_wrapper_test.go
+++ cache_wrapper/cache_wrapper_test.go
@@ -7,10 +7,36 @@ import (
 
 	"github.com/preston-wagner/go-cache/cache_wrapper"
 	"github.com/preston-wagner/go-cache/ttl_cache"
+	"github.com/preston-wagner/unicycle/sets"
 )
 
+func itoaOnce(t *testing.T) func(int) string {
+	keys := sets.Set[int]{}
+	return func(key int) string {
+		if keys.Has(key) {
+			t.Error("wrapped function was called with the same key multiple times!")
+		}
+		keys.Add(key)
+		return strconv.Itoa(key)
+	}
+}
+
+func atoiOnce(t *testing.T) func(string) (int, error) {
+	keys := sets.Set[string]{}
+	return func(key string) (int, error) {
+		if keys.Has(key) {
+			t.Error("wrapped function was called with the same key multiple times!")
+		}
+		keys.Add(key)
+		return strconv.Atoi(key)
+	}
+}
+
 func TestWrapWithCache(t *testing.T) {
-	wrappedGetter := cache_wrapper.WrapWithCache[int, string](strconv.Itoa, ttl_cache.NewTTLCache[int, string](time.Second, time.Second*3))
+	wrappedGetter := cache_wrapper.WrapWithCache[int, string](
+		itoaOnce(t),
+		ttl_cache.NewTTLCache[int, string](time.Second, time.Second*3),
+	)
 	res := wrappedGetter(7)
 	if res != "7" {
 		t.Error("WrapWithCache wrapped getter returned wrong value")
@@ -22,7 +48,10 @@ func TestWrapWithCache(t *testing.T) {
 }
 
 func TestWrapWithCacheWithUncachedError(t *testing.T) {
-	wrappedGetter := cache_wrapper.WrapWithCacheWithUncachedError[string, int](strconv.Atoi, ttl_cache.NewTTLCache[string, int](time.Second, time.Second*3))
+	wrappedGetter := cache_wrapper.WrapWithCacheWithUncachedError[string, int](
+		atoiOnce(t),
+		ttl_cache.NewTTLCache[string, int](time.Second, time.Second*3),
+	)
 	res, err := wrappedGetter("7")
 	if err != nil {
 		t.Error(err)
@@ -40,5 +69,35 @@ func TestWrapWithCacheWithUncachedError(t *testing.T) {
 	_, err = wrappedGetter("A")
 	if err == nil {
 		t.Error("WrapWithCacheWithUncachedError should return an error when the getter does")
+	}
+}
+
+func TestWrapWithCacheWithError(t *testing.T) {
+	wrappedGetter := cache_wrapper.WrapWithCacheWithError[string, int](
+		atoiOnce(t),
+		ttl_cache.NewTTLCache[string, int](time.Second, time.Second*3),
+		ttl_cache.NewTTLCache[string, error](time.Second, time.Second*3),
+	)
+	res, err := wrappedGetter("7")
+	if err != nil {
+		t.Error(err)
+	}
+	if res != 7 {
+		t.Error("WrapWithCacheWithError wrapped getter returned wrong value")
+	}
+	res, err = wrappedGetter("7")
+	if err != nil {
+		t.Error(err)
+	}
+	if res != 7 {
+		t.Error("WrapWithCacheWithError wrapped getter returned wrong cached value")
+	}
+	_, err = wrappedGetter("A")
+	if err == nil {
+		t.Error("WrapWithCacheWithError should return an error when the getter does")
+	}
+	_, err = wrappedGetter("A")
+	if err == nil {
+		t.Error("WrapWithCacheWithError wrapped getter returned wrong cached error")
 	}
 }
