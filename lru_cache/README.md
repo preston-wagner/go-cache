542b711ac2ad95f6216e4b36f7aa9156e32b6316
Preston Wagner <2636532+preston-wagner@users.noreply.github.com>
2023-11-02 17:57:40 -0600

    lru cache WIP

---
+++ lru_cache/README.md
@@ -0,0 +1,4 @@
+# Least-Recently Used (LRU) Cache
+https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_recently_used_(LRU)
+Values are removed from the cache when:
+- the cache detects that the provided memory usage threshold has been exceeded, and the value is older than the average of all current values
---
+++ lru_cache/lru_cache_test.go
@@ -0,0 +1,46 @@
+package lru_cache_test
+
+// import (
+// 	"testing"
+// 	"time"
+// )
+
+// func TestTLRUCache(t *testing.T) {
+// 	// test purging based on TTL expiration
+// 	TLRUCache := lru_cache.NewTLRUCache[int, string](time.Second, 1)
+
+// 	TLRUCache.Set(1, "1")
+// 	if value, ok := TLRUCache.Get(1); value != "1" || !ok {
+// 		t.Error("TLRUCache.Get failed")
+// 	}
+
+// 	time.Sleep(time.Second * 2)
+
+// 	if _, ok := TLRUCache.Get(1); ok {
+// 		t.Error("TLRUCache.Get should not return ok for expired values")
+// 	}
+
+// 	TLRUCache.StopReaping()
+
+// 	// test purging based on memory usage
+// 	TLRUCache = NewTLRUCache[int, string](time.Second*5, 0)
+
+// 	TLRUCache.Set(1, "1")
+// 	TLRUCache.Set(2, "2")
+// 	TLRUCache.Set(3, "3")
+// 	if value, ok := TLRUCache.Get(1); value != "1" || !ok {
+// 		t.Error("TLRUCache.Get failed")
+// 	}
+
+// 	time.Sleep(time.Second * 2)
+
+// 	if _, ok := TLRUCache.Get(1); ok {
+// 		t.Error("TLRUCache.Get should not return ok for values purged by reaper")
+// 	}
+// 	// the 2nd value may or may not be reaped based on the timing of the test, but the first should always be reaped and the last should never be
+// 	if _, ok := TLRUCache.Get(3); !ok {
+// 		t.Error("TLRUCache.Get should return ok for values more recent than the average after reaping")
+// 	}
+
+// 	TLRUCache.StopReaping()
+// }
